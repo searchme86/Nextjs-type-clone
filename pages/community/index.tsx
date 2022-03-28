20ea4ab88d0795730ef418c21416424edc3922a0
searchme86 <searchme86@gmail.com>
2022-03-28 21:08:33 +0900

    🥅  Catch Error : null vale of latitude&longtitude

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -4,7 +4,7 @@ import FloatingButton from '@components/floating-button';
 import Layout from '@components/layout';
 import useSWR from 'swr';
 import { Post, User } from '@prisma/client';
-
+import useCoords from '@libs/client/useCoords';
 interface PostWithUser extends Post {
   user: User;
   _count: {
@@ -12,14 +12,17 @@ interface PostWithUser extends Post {
     answers: number;
   };
 }
-
 interface PostsResponse {
   ok: boolean;
   posts: PostWithUser[];
 }
-
 const Community: NextPage = () => {
-  const { data } = useSWR<PostsResponse>(`/api/posts`);
+  const { latitude, longitude } = useCoords();
+  const { data } = useSWR<PostsResponse>(
+    latitude && longitude
+      ? `/api/posts?latitude=${latitude}&longitude=${longitude}`
+      : null
+  );
   return (
     <Layout hasTabBar title="동네생활">
       <div className="space-y-4 divide-y-[2px]">
