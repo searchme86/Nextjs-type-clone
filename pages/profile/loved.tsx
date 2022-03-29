52ab9d2a1eb684a1f65ab0b20b6201d75c8e64b4
searchme86 <searchme86@gmail.com>
2022-03-29 19:34:10 +0900

    🚧  edited :  components applied

--- pages/profile/loved.tsx
+++ pages/profile/loved.tsx
@@ -1,24 +1,15 @@
 import type { NextPage } from 'next';
-import Item from '../../components/item';
-import Layout from '../../components/layout';
+import Item from '@components/item';
+import Layout from '@components/layout';
+import ProductList from '@components/product-list';
 
 const Loved: NextPage = () => {
   return (
     <Layout title="관심목록" canGoBack>
       <div className="flex flex-col space-y-5 divide-y  pb-10">
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Item
-            key={i}
-            id={i}
-            title="iPhone 14"
-            price={99}
-            comments={1}
-            hearts={1}
-          />
-        ))}
+        <ProductList kind="favs" />
       </div>
     </Layout>
   );
 };
-
 export default Loved;
